f6ec6a09ed631e46d3b4be64329577e79d3ed515
Bronts <bronts@163.com>
2018-01-01 11:13:27 +0800

    添加项目文件。

---
+++ Identity2MySql/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="Identity2MySql.MvcApplication" Language="C#" %>
---
+++ Identity2MySql/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(Identity2MySql.Startup))]
+namespace Identity2MySql
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
